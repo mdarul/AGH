9418a6165f686cd8d8d742379945c00b6cf24745
mdarul@outlook.com <mdarul618@gmail.com>
2017-11-17 15:29:53 +0000

    fixed tw_hw4

--- 5th-semester/TW/lab4-homework/Executor.java
+++ 5th-semester/TW/lab4-homework/Executor.java
@@ -3,7 +3,7 @@
  */
 public class Executor {
     public static void main(String[] args){
-        int N=5, M=6;
+        int N=6, M=6;
         Node[][] nodes = new Node[N][M];
 
         for(int i=0; i<N; i++)
@@ -17,29 +17,56 @@ public class Executor {
         try {p0.join();}
         catch (InterruptedException ex) {}
 
-        //P1
-        P1[] p1 = new P1[M-1];
-        for(int i=1; i<M; i++) p1[i-1] = new P1(nodes[0][i-1], nodes[0][i]);
-        for(int i=0; i<M-1; i++) p1[i].start();
-        try {
-            for(int i=0; i<M-1; i++) p1[i].join();
+        //P1, P2
+        P1 p1;
+        P2 p2;
+        int middle_M = (M-1) / 2;
+        int middle_N = (N-1) / 2;
+        for(int i=0; i < middle_M; i++) {
+            p1 = new P1(nodes[middle_N][middle_M + i], nodes[middle_N][middle_M + i+1]);
+            p2 = new P2(nodes[middle_N][middle_M - i], nodes[middle_N][middle_M - (i+1)]);
+            p1.start();
+            p2.start();
+            try {
+                p1.join();
+                p2.join();
+            }
+            catch (InterruptedException ex) {}
         }
-        catch (InterruptedException ex) {}
+        //P1 last
+        if(M%2 == 0) {
+            p1 = new P1(nodes[middle_N][M-2], nodes[middle_N][M-1]);
+            p1.start();
+            try {p1.join();}
+            catch (InterruptedException ex) {}
+        }
 
-        // P3
+        // P3, P4
         P3[] p3 = new P3[M];
-        for(int i=0; i < N-1; i++) {
-            for(int j=0; j<M; j++) p3[j] = new P3(nodes[i][j], nodes[i+1][j]);
+        P4[] p4 = new P4[M];
+        for(int i=0; i < middle_N; i++) {
+            for(int j=0; j<M; j++) p3[j] = new P3(nodes[middle_N + i][j], nodes[middle_N + i+1][j]);
+            for(int j=0; j<M; j++) p4[j] = new P4(nodes[middle_N - i][j], nodes[middle_N - (i+1)][j]);
             for(int j=0; j<M; j++) p3[j].start();
+            for(int j=0; j<M; j++) p4[j].start();
             try {
                 for(int j=0; j < M; j++) p3[j].join();
+                for(int j=0; j < M; j++) p4[j].join();
             }
+            catch (InterruptedException ex) {}
+        }
+        //P3 last
+        if(N%2 == 0) {
+            for(int j=0; j<M; j++) p3[j] = new P3(nodes[N-2][j], nodes[N-1][j]);
+            for(int j=0; j<M; j++) p3[j].start();
+            try {for(int j=0; j < M; j++) p3[j].join();}
             catch (InterruptedException ex) {}
         }
 
         // P5
         P5[] p5 = new P5[M-1];
-        for(int i=1; i < N; i++) {
+        for(int i=0; i < N; i++) {
+            if(i == middle_N) continue; // we have already set row connection for cells in middle_N (operations P1 and P2)
             for(int j=0; j<M-1; j++) p5[j] = new P5(nodes[i][j], nodes[i][j+1]);
             for(int j=0; j<M-1; j++) p5[j].start();
             try {
